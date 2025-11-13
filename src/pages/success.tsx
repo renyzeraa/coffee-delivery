783ecece3d59f13c2d95a768cb997e5f27cfa957
Renan Silva <renansilvaytB@gmail.com>
2025-11-13 15:47:00 -0300

    feat: success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,84 @@
+import { CurrencyDollarIcon, MapPinIcon, TimerIcon } from '@phosphor-icons/react'
+import { useParams } from 'react-router-dom'
+import { useCartStore } from '../store/cart'
+import { useShallow } from 'zustand/shallow'
+
 export function Success() {
+    const { orders } = useCartStore(useShallow(state => ({
+        orders: state.orders
+    })))
+    const { orderId } = useParams()
+    const orderInfo = orders.find((order) => order.id === Number(orderId))
+    const paymentMethod = {
+        credit: 'Cartão de crédito',
+        debit: 'Cartão de débito',
+        cash: 'Dinheiro',
+    }
+
+    if (!orderInfo?.id) {
+        return null
+    }
+
     return (
-        <h1>Success</h1>
+        <main className='flex max-w-[1160px] mx-auto py-[80px] px-5 items-end justify-between gap-10'>
+            <div className='flex flex-col gap-10'>
+                <div className='flex flex-col gap-1'>
+                    <h2 className='text-titleL font-baloo-2 font-bold text-yellow-dark'>Uhu! Pedido confirmado</h2>
+                    <span className='text-textL text-base-subtitle'>Agora é só aguardar que logo o café chegará até você</span>
+                </div>
+
+                <div className='border border-solid border-transparent w-full bg-origin-border bg-gradient-yellow-purple'>
+                    <div className='p-10 bg-white rounded-tr-[36px] rounded-bl-[36px] flex flex-col gap-8'>
+                        <div className='flex items-center gap-3'>
+                            <MapPinIcon
+                                className='text-white bg-purple p-2 rounded-full'
+                                size={32}
+                            />
+
+                            <div className='flex flex-col'>
+                                <span>
+                                    Entrega em{' '}
+                                    <strong>
+                                        {orderInfo.street}, {orderInfo.number}
+                                    </strong>
+                                </span>
+
+                                <span>
+                                    {orderInfo.neighborhood} - {orderInfo.city},{orderInfo.state}
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className='flex items-center gap-3'>
+                            <TimerIcon
+                                className='text-white bg-yellow p-2 rounded-full'
+                                size={32}
+                            />
+
+                            <div className='flex flex-col'>
+                                <span>Previsão de entrega</span>
+
+                                <strong>20 min - 30 min</strong>
+                            </div>
+                        </div>
+
+                        <div className='flex items-center gap-3'>
+                            <CurrencyDollarIcon
+                                className='text-white bg-yellow-dark p-2 rounded-full'
+                                size={32}
+                            />
+
+                            <div className='flex flex-col'>
+                                <span>Pagamento na entrega</span>
+
+                                <strong>{paymentMethod[orderInfo.paymentMethod]}</strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <img className='-mb-[13px] block' src="/images/delivery.svg" alt="Pedido concluído" />
+        </main>
     )
 }
